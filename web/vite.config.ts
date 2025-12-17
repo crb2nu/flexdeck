557c30955c6bf784e16305fa9666be2291d4ac79
Cody Blevins <cody@example.com>
2025-12-17 18:56:58 -0500

    chore: Add parent directory to workspace and configure Vite base path to relative.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "./",
   plugins: [solidPlugin()],
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
         changeOrigin: true,
       },
     },
   },
   build: {
-    target: 'esnext',
-    outDir: 'dist',
+    target: "esnext",
+    outDir: "dist",
   },
 });
